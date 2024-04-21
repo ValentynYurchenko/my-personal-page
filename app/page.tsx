b923f87858a93154d878013902ae333c16a41779
ValentynYurchenko <valentin.urchenko@gmail.com>
2024-04-21 20:14:06 +0300

    change favicon and add my photo

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,18 @@ import Link from 'next/link';
 import styles from './page.module.css';
 import { withLayout } from '@/layout/Layout';
 import Image from 'next/image';
-import photo from '../layout/photo.png';
+import myphoto from '../layout/icons/15-removebg-preview.png';
 
 function Home() {
-  return <div className={styles.main}></div>;
+  return (
+    <div className={styles.main}>
+      <Image src={myphoto} alt="photo" />
+      <h2>
+        I`m <span>Valentyn Yurchenko</span> <br />
+        JAVASCRIPT DEVELOPER
+      </h2>
+    </div>
+  );
 }
 
 export default withLayout(Home);
